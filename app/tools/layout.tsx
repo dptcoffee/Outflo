e4e2db6581d0daf56d521705b9bf8e6a13a87a24
dptcoffee <e.will.to.believe@gmail.com>
2026-02-25 15:29:50 -0500

    Gate /login behind Portal pass (via=portal)

--- app/tools/layout.tsx
+++ app/tools/layout.tsx
@@ -10,7 +10,7 @@ export default async function ProtectedLayout({
   const { data } = await supabase.auth.getUser();
 
   if (!data.user) {
-    redirect("/login");
+    redirect("/");
   }
 
   return <>{children}</>;
